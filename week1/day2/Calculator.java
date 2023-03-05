d58293e0d8961cf00cf167d8dc06bb9986e78fda
DharaniThangarasu <82213787+DharaniThangarasu@users.noreply.github.com>
2023-03-05 18:36:57 +0530

    Add files via upload
    Week1, Day2

---
+++ week1/day2/Calculator.java
@@ -0,0 +1,16 @@
+package week1.day2;
+
+public class Calculator {
+	
+	public int add(int a , int b , int c) 
+	{
+		return a+b+c;
+	}
+	public int multiply(float a, float b)
+	{
+		return (int) (a*b);
+	}
+	public void sub() {
+		System.out.println("5-3 =" + (5-3));
+	}
+}
---
+++ week1/day2/MyCalculator.java
@@ -0,0 +1,12 @@
+package week1.day2;
+
+public class MyCalculator {
+
+	public static void main(String[] args) {
+		Calculator obj = new Calculator();
+		System.out.println(obj.add(1, 5,18 ));
+		System.out.println(obj.multiply(13.45f, 18.9f));
+		obj.sub();
+	}
+
+}
