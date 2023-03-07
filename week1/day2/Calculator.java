4a7bea66f6e94163122c8c3847a61aae44c84118
DharaniThangarasu <82213787+DharaniThangarasu@users.noreply.github.com>
2023-03-07 19:42:20 +0530

    Update Calculator.java
    Added comments

--- week1/day2/Calculator.java
+++ week1/day2/Calculator.java
@@ -1,16 +1,18 @@
 package week1.day2;
 
 public class Calculator {
-	
-	public int add(int a , int b , int c) 
-	{
-		return a+b+c;
+	// Addition method.
+	public int add(int a, int b, int c) {
+		return a + b + c;
 	}
-	public int multiply(float a, float b)
-	{
-		return (int) (a*b);
+
+	// Multiplication method.
+	public int multiply(float a, float b) {
+		return (int) (a * b);
 	}
+
+	// Subtraction Method
 	public void sub() {
-		System.out.println("5-3 =" + (5-3));
+		System.out.println("5-3 =" + (5 - 3));
 	}
 }
