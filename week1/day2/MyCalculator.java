1970ea0fac9e248ef9d5b828c52dc59d28e81691
DharaniThangarasu <82213787+DharaniThangarasu@users.noreply.github.com>
2023-03-07 19:49:01 +0530

    Update MyCalculator.java
    Added comments

--- week1/day2/MyCalculator.java
+++ week1/day2/MyCalculator.java
@@ -6,9 +6,9 @@ public class MyCalculator {
 		// Create an object for the class Calculator.
 		Calculator obj = new Calculator();
 		// Access the addition method and print the values.
-		System.out.println(obj.add(1, 5, 18));
+		System.out.println("Addition:" + obj.add(1, 5, 18));
 		// Access the multiplication method and print the values.
-		System.out.println(obj.multiply(13.45f, 18.9f));
+		System.out.println("Multiplication: " + obj.multiply(13.45f, 18.9f));
 		// Access the subtraction method.
 		obj.sub();
 	}
