3cc9e07df2e239a28c8be3cf4895b8e23569c4a3
DharaniThangarasu <82213787+DharaniThangarasu@users.noreply.github.com>
2023-03-07 19:40:02 +0530

    Update PrimeNumber.java
    Added comments

--- week1/day1/PrimeNumber.java
+++ week1/day1/PrimeNumber.java
@@ -3,31 +3,27 @@ package week1.day1;
 public class PrimeNumber {
 
 	public static void main(String[] args) {
-//		Iterate through all numbers from 2 to n-1 (input) and 
-//		for every number check if it divides n (input). 
-//		If we find any number that divides, print prime.
-//		If nothing divides, then print non-prime
-		int n = -15;
+		// Input value.
+		int n = 15;
 		boolean isPrime = true;
-		if (n <= 1) 
-		{
+		// If the input number is less than or equal to 1, the number is not a prime number. Set isPrime to false.
+		if (n <= 1)
 			isPrime = false;
-		} 
-		else 
-		{
-			for (int i = 2; i < n; i++) 
-			{
-				if (n % i == 0) 
-				{
+		else {
+			// Iterate from 2 until the input number.
+			for (int i = 2; i < n; i++) {
+				// If the input number is divisible by other numbers , then the number is not a prime number. Set isPrime to false.
+				if (n % i == 0) {
 					isPrime = false;
 					break;
 				}
 			}
 		}
+		// Check the boolean isPrime and print the output
 		if (isPrime)
-				System.out.println(n + " is a prime number");
+			System.out.println(n + " is a prime number");
 		else
-				System.out.println(n + " is not a prime number");
-		}
+			System.out.println(n + " is not a prime number");
+	}
 
 }
