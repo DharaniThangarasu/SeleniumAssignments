bcd6dd1527d4b3881cf6f322c45da6b5529e8b55
DharaniThangarasu <82213787+DharaniThangarasu@users.noreply.github.com>
2023-03-07 19:41:05 +0530

    Update Vehicle.java
    Added comments

--- week1/day1/Vehicle.java
+++ week1/day1/Vehicle.java
@@ -3,13 +3,21 @@ package week1.day1;
 public class Vehicle {
 
 	public static void main(String[] args) {
+		// Integer
 		int noOfWheels = 4;
+		// Character
 		char logo = 'M';
+		// String
 		String carName = "Maruthi Swift";
+		// Boolean
 		boolean isNew = true;
+		// Float
 		float mileage = 35.8f;
+		// Long.
 		long number = 1234596789l;
+		// Double
 		double kilometer = 950.58778;
+		// Print the values.
 		System.out.println("Vehicale Details:");
 		System.out.println("No of wheels:" + noOfWheels);
 		System.out.println("Logo:" + logo);
@@ -18,7 +26,6 @@ public class Vehicle {
 		System.out.println("Mileage:" + mileage);
 		System.out.println("Number:" + number);
 		System.out.println("Kilometer:" + kilometer);
-
 	}
 
 }
