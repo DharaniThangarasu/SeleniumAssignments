0e7d7aa79e780e2e4e1f2fa6db2736d3eaa7cba3
DharaniThangarasu <82213787+DharaniThangarasu@users.noreply.github.com>
2023-03-07 19:43:18 +0530

    Update FindDuplicate.java
    Added Comments

--- week1/day2/FindDuplicate.java
+++ week1/day2/FindDuplicate.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class FindDuplicate {
 
 	public static void main(String[] args) {
+		// Input array
 		int[] nums = { 2, 5, 7, 7, 5, 9, 2, 3 };
 		System.out.println("Duplicate elements in given array: ");
 		//Method 1:
